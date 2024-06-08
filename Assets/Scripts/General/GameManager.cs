978227906ca85500d5c0b9728cbd3e87f87b0f20
Abdulrahman <abdalrahmanabs2005@gmail.com>
2024-06-08 18:00:31 +0300

    Setup audio manager And Game Manager

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,19 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 public class GameManager : Singleton<GameManager>
 {
-   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public UnityEvent OnLose;
+    public UnityEvent OnLost;
+    public UnityEvent OnPause;
+
+
+    private int currentSceneCount = 0;
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void LoadScene(int buildIndex) { }
+    public void ReloadScene()
+    {
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        currentSceneCount++;
+    }
+
 }
+
+   
+
